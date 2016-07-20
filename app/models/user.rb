88c3ac227f0090f9ff1c46ba75698e61127dbbcd
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-07-19 20:39:55 -0700

    Write associations for user. :construction:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  include BCrypt
+
+  has_many :rsvps
+  has_many :events, through: :rsvps
+  has_many :created_events, :class_name => "Event", foreign_key: "creator_id"
 end
