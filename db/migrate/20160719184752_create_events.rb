9273bb30556bbc6a7bf5be00baec20fa0ba6e8af
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-07-19 20:42:22 -0700

    Edit events and rsvp tables. :construction:

--- db/migrate/20160719184752_create_events.rb
+++ db/migrate/20160719184752_create_events.rb
@@ -1,4 +1,12 @@
 class CreateEvents < ActiveRecord::Migration
   def change
+  	create_table :events do |t|
+  		t.string :name, :null => false
+  		t.datetime :date, :null => false
+  		t.string :category, :null => false
+  		t.integer :creator_id, :null => false
+
+  		t.timestamps(null: false)
+  	end
   end
 end
--- db/migrate/20160719185836_create_rsvps.rb
+++ db/migrate/20160719185836_create_rsvps.rb
@@ -1,4 +1,11 @@
 class CreateRsvps < ActiveRecord::Migration
   def change
+  	create_table :rsvps do |t|
+  		t.integer :attendee_id, :null => false
+  		t.integer :event_id, :null => false
+  	
+  		t.timestamps(null: false)
+
+  	end
   end
 end
