4c2ba7fe19885ec291a9871a84f18813bd86aa9d
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-07-20 15:45:00 -0700

    Add 'chronic' gem. :construction:

--- db/migrate/20160719184752_create_events.rb
+++ db/migrate/20160719184752_create_events.rb
@@ -2,8 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
   	create_table :events do |t|
   		t.string :name, :null => false
-  		t.date :date, :null => false
-  		t.time :time
+  		t.datetime :date,
   		t.string :category, :null => false
   		t.integer :creator_id, :null => false
 
