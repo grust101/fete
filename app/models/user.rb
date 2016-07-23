8e9e350ab1ff550030f07dc95ba464593fc0e32f
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-07-22 19:34:53 -0700

    Write bcrypt enabler for password_digest. :construction:

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   has_many :rsvps, foreign_key: :attendee_id
   has_many :events_to_attend, through: :rsvps
   has_many :created_events, :class_name => "Event", foreign_key: "creator_id"
+
+  has_secure_password
 end
