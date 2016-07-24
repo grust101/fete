c0d96fc37450d68d5e8280587fe0ce638ee5b890
Georgia Rust <email": null>
2016-07-24 12:22:47 -0700

    Add details page for event. Need to continue this feature. :heavy_plus_sign:

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -7,9 +7,9 @@ post '/events' do
 
  @event = Event.new(params[:event])
  @event.creator_id = current_user.id
- 
+
  if @event.save
-		redirect "/users/#{current_user.id}"
+		redirect "/events/:id"
 	else
 		@error = "could not create event"
 		p @error
@@ -18,6 +18,7 @@ post '/events' do
 
 end
 
-get '/events/show' do
-  "Hello World"
+get '/events/:id' do
+  @event = Event.find_by(params[:name])
+  erb :'/events/details'
 end
